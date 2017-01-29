e8057a5eba41ec29f4d5b4fad33a3023fdbccc1d
Topbit Du <topbit.du@gmail.com>
2017-01-29 10:29:45 +0800

    1, Models RSpec examples manifest.

---
+++ lib/unidom/certificate/china/models_rspec.rb
@@ -0,0 +1,2 @@
+require 'rspec/models/unidom/certificate/china/business_license_spec' unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Certificate::China::BusinessLicense'
+require 'rspec/models/unidom/certificate/china/identity_card_spec'    unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Certificate::China::IdentityCard'
