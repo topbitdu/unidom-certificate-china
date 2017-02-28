9430e4dbeef4d416a68859ad2aa3e9677203d98d
Topbit Du <topbit.du@gmail.com>
2017-02-28 18:22:47 +0800

    1, Improve the Business License spec for the validations on the #registration_number attribute, & the #unified_social_credit_identifier attribute.

--- lib/rspec/models/unidom/certificate/china/business_license_spec.rb
+++ lib/rspec/models/unidom/certificate/china/business_license_spec.rb
@@ -19,7 +19,44 @@ describe Unidom::Certificate::China::BusinessLicense, type: :model do
       validity_thru_date:               Date.current+10.years
     }
 
+    registration_number_max_length              = described_class.columns_hash['registration_number'].limit
+    unified_social_credit_identifier_max_length = described_class.columns_hash['unified_social_credit_identifier'].limit
+
     it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
+
+    it_behaves_like 'validates', model_attributes, :registration_number,
+      {                            } => 0,
+      { registration_number: nil   } => 0,
+      { registration_number: ''    } => 0,
+      { registration_number: '1'   } => 2,
+      { registration_number: 'A'   } => 3,
+      { registration_number: '11'  } => 2,
+      { registration_number: 'AA'  } => 3,
+      { registration_number: '111' } => 2,
+      { registration_number: 'AAA' } => 3,
+      { registration_number: '1'*(registration_number_max_length-1) } => 2,
+      { registration_number: 'A'*(registration_number_max_length-1) } => 3,
+      { registration_number: '1'*registration_number_max_length     } => 0,
+      { registration_number: 'A'*registration_number_max_length     } => 2,
+      { registration_number: '1'*(registration_number_max_length+1) } => 2,
+      { registration_number: 'A'*(registration_number_max_length+1) } => 3
+
+    it_behaves_like 'validates', model_attributes, :unified_social_credit_identifier,
+      {                                         } => 0,
+      { unified_social_credit_identifier: nil   } => 0,
+      { unified_social_credit_identifier: ''    } => 0,
+      { unified_social_credit_identifier: '1'   } => 1,
+      { unified_social_credit_identifier: 'A'   } => 1,
+      { unified_social_credit_identifier: '11'  } => 1,
+      { unified_social_credit_identifier: 'AA'  } => 1,
+      { unified_social_credit_identifier: '111' } => 1,
+      { unified_social_credit_identifier: 'AAA' } => 1,
+      { unified_social_credit_identifier: '1'*(unified_social_credit_identifier_max_length-1) } => 1,
+      { unified_social_credit_identifier: 'A'*(unified_social_credit_identifier_max_length-1) } => 1,
+      { unified_social_credit_identifier: '1'*unified_social_credit_identifier_max_length     } => 0,
+      { unified_social_credit_identifier: 'A'*unified_social_credit_identifier_max_length     } => 0,
+      { unified_social_credit_identifier: '1'*(unified_social_credit_identifier_max_length+1) } => 1,
+      { unified_social_credit_identifier: 'A'*(unified_social_credit_identifier_max_length+1) } => 1
 
   end
 
