f39b8dad24147ba4aafedc0183afc5f8bc2a7333
Topbit Du <topbit.du@gmail.com>
2017-03-03 19:42:39 +0800

    1, Improve the Application controller for the namespace.

--- app/controllers/unidom/certificate/china/application_controller.rb
+++ app/controllers/unidom/certificate/china/application_controller.rb
@@ -1,6 +1,6 @@
-module China
-  class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-end
+##
+# Application controller 是模块内所有控制器的基类。
 
+class Unidom::Certificate::China::ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
 end
